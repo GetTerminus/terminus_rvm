23dfa4b00ec0b31bc480030425b6336ae96902b7
Seth Floyd <seth@sethfloydjr.com>
2017-02-22 15:33:39 -0500

    changes

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,7 +10,8 @@
 	  	#user 'root'
 	  	#cwd '/tmp'
 	  	code <<-EOH
-		\curl -sSL https://get.rvm.io | bash -s stable
+		gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+		curl -sSL https://get.rvm.io | bash -s stable
 		source /home/ubuntu/.rvm/scripts/rvm
 		rvm install ruby-2.1.7
 		rvm --default use 2.1.7
