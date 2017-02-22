2251644281fc4830f6b8a95ddac4af10f71117c8
Seth Floyd <seth@sethfloydjr.com>
2017-02-22 13:17:30 -0500

    no apt-get

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,8 +10,6 @@
 	  	user 'root'
 	  	cwd '/tmp'
 	  	code <<-EOH
-		apt-get update
-		apt-get -y upgrade
 		gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 		\curl -sSL https://get.rvm.io | bash -s stable
 		source /home/ubuntu/.rvm/scripts/rvm
