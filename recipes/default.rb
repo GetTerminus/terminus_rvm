d97b31d294ab5821c77fcc0810af0d4d113628db
Seth Floyd <seth@sethfloydjr.com>
2017-02-22 15:40:15 -0500

    changes

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,7 +12,7 @@
 	  	code <<-EOH
 		gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 		curl -sSL https://get.rvm.io | bash -s stable
-		source /home/ubuntu/.rvm/scripts/rvm
+		source /etc/profile.d/rvm.sh
 		rvm install ruby-2.1.7
 		rvm --default use 2.1.7
 		rvm install --gems=rails,puma
