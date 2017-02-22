34109eccb7c9ab88f469bc2704aa61d21c9be30e
Seth Floyd <seth@sethfloydjr.com>
2017-02-22 14:58:12 -0500

    no apt-get

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,7 +10,7 @@
 	  	user 'root'
 	  	cwd '/tmp'
 	  	code <<-EOH
-		gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+		#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 		\curl -sSL https://get.rvm.io | bash -s stable
 		source /home/ubuntu/.rvm/scripts/rvm
 		rvm install ruby-2.1.7
