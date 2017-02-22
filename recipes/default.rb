5a72c0400867de9c484260f85e5ce008507cfd16
Seth Floyd <seth@sethfloydjr.com>
2017-02-22 12:49:37 -0500

    initial run

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,20 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+
+	bash 'postgresql-95-ubuntu' do
+	  	user 'root'
+	  	cwd '/tmp'
+	  	code <<-EOH
+		apt-get update
+		apt-get -y upgrade
+		gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+		\curl -sSL https://get.rvm.io | bash -s stable
+		source /home/ubuntu/.rvm/scripts/rvm
+		rvm install ruby-2.1.7
+		rvm --default use 2.1.7
+		rvm install --gems=rails,puma
+	  	EOH
+	end
